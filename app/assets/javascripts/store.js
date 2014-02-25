5e4b7619c62200af8d9ec01845c78471a660cc71
shrikanthkr <shrikanth_kr@yahoo.com>
2014-02-25 17:05:51 +0530

    initial commit

---
+++ app/assets/javascripts/store.js
@@ -0,0 +1,9 @@
+EmberLearn.Store = DS.Store.extend({
+
+});
+
+// Override the default adapter with the `DS.ActiveModelAdapter` which
+// is built to work nicely with the ActiveModel::Serializers gem.
+EmberLearn.ApplicationAdapter = DS.ActiveModelAdapter.extend({
+
+});
